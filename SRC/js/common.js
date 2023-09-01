ada0d45145f35d3f62b9984e5490d67238057d79
masami yamashiro <202305ap01_yamashiro@x-die.sakura.ne.jp>
2023-09-01 14:01:36 +0900

    工事管理表バグＮｏ145修正後、初回コミット＆プッシュ

--- SRC/js/common.js
+++ SRC/js/common.js
@@ -5,22 +5,17 @@
  * @return true:正常、false:異常
  */
 function fnYMDCheck(msg, obj) {
-
 	// 未入力時はチェックしない
 	oYMD = obj.value;
 	if (!oYMD) { return true; }
-
 	var tmp = oYMD.split('/');
 	var ymd = new Date(tmp[0], parseInt(tmp[1], 10) - 1, parseInt(tmp[2], 10));
 	var vYMD = ymd.getFullYear() + '/' + ('0' + (ymd.getMonth() + 1)).slice(-2) + '/' + ('0' + ymd.getDate()).slice(-2);
-	if (oYMD.length) {
-		if (oYMD == vYMD) {
-			return true;
-		} else {
-			alert(msg + "を入力してください");
-			return false;
-		}
-
+	if (oYMD == vYMD) {
+		return true;
+	} else {
+		alert(msg + "を入力してください");
+		return false;
 	}
 }
 
@@ -37,10 +32,10 @@ function isLength(length, msg, obj) {
 	rtn = false;
 	if (obj.value.length > length) {
 		alert(msg + "は" + length + "文字以内で入力して下さい");
+		rtn = true; // ← 追加
 	}
 	return rtn;
 }
-
 
 
 /**
@@ -53,7 +48,7 @@ function isLength(length, msg, obj) {
  */
 function isNumericLength(length, msg, obj) {
 	rtn = false;
-	if (obj.value.length > 9 || obj.value.match(/[^0-8]+/)) {
+	if (obj.value.length > length || obj.value.match(/[^0-9]+/)) {
 		alert(msg + "は" + length + "桁以内の半角数字で入力してください");
 		rtn = true;
 	}
